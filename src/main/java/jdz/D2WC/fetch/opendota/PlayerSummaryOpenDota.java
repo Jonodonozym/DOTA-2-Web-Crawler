228939df3548c61f067282f0307bf2adb3b2aa9e
bakerjaid <jaidencolebaker@gmail.com>
2019-05-28 14:34:34 +1200

    Fixed empty summaries not fetching correctly

--- src/main/java/jdz/D2WC/fetch/opendota/PlayerSummaryOpenDota.java
+++ src/main/java/jdz/D2WC/fetch/opendota/PlayerSummaryOpenDota.java
@@ -16,6 +16,8 @@ public class PlayerSummaryOpenDota extends JSONParser<JSONObject> implements Pla
 	public PlayerSummary fromPlayerID(long playerID) throws IOException {
 		JSONObject MMREstimate = readJSONFromUrl(OpenDotaAPI.URL() + "players/" + playerID)
 				.getJSONObject("mmr_estimate");
+		if (!MMREstimate.has("estimate"))
+			return new PlayerSummary(playerID, 0, 0, 0, 0, false, 0);
 		Pair<Integer, Integer> winLoss = getWinLoss(playerID);
 		return new PlayerSummary(playerID, MMREstimate.getInt("estimate"),
 				MMREstimate.has("stdDev") ? MMREstimate.getInt("stdDev") : 0, winLoss.getKey(), winLoss.getValue(),
